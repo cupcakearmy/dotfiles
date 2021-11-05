d980651c7421aa54cef10ad4876e42c8b74647f3
Niccolo Borgioli <niccolo.borgioli@lovoo.com>
2021-11-05 16:09:08 +0100

    only execute brew if installed

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -3,7 +3,9 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -x PIPENV_VENV_IN_PROJECT true
 
 export GOPATH="$HOME/.go"
-export GOROOT=(brew --prefix go)"/libexec"
+if type -q $program
+  export GOROOT=(brew --prefix go)"/libexec"
+end
 export PATH="$PATH:$GOPATH/bin:$GOROOT/bin:$HOME/.cargo/bin"
 
 # The next line updates PATH for the Google Cloud SDK.
