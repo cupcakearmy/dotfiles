2ee671201c3179a5a82db933a513949196ee0e76
cupcakearmy <hi@nicco.io>
2022-10-26 15:59:19 +0200

    rename docker system prune

--- files/fish/functions/dpa.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function dpa --wraps='docker system prune -af --volumes' --description 'alias dpa docker system prune -af --volumes'
-  docker system prune -af --volumes $argv;
-end
