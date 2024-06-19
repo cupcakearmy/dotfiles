11db715f7240a3dad864ecde7fe095b56d754179
Niccolo Borgioli <hi@nicco.io>
2024-06-19 10:03:56 +0200

    cleanup brew and also update casks

--- files/fish/functions/brewer.fish
+++ files/fish/functions/brewer.fish
@@ -1,3 +1,3 @@
 function brewer --wraps='brew update && brew upgrade && brew cleanup' --description 'alias brewer brew update && brew upgrade && brew cleanup'
-  brew update && brew upgrade && brew cleanup $argv; 
+  brew update && brew upgrade && brew upgrade --cask $(brew list --cask) && brew cleanup $argv; 
 end
